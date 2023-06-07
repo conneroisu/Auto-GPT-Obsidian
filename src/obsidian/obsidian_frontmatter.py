2cc6eb647e016305ede89bba058c0b4996225951
connero <conneroisu@outlook.com>
2023-06-06 23:57:32 -0500

    Revert "Updated Offline work"
    This reverts commit 862f1962728ba435c8e9431692dee7a461fe782e.

--- src/obsidian/obsidian_frontmatter.py
+++ src/obsidian/obsidian_frontmatter.py
@@ -1,69 +1,34 @@
-""" Import the operating system module to allow for operating system dependent functionality. """
 import os
 
-class Obsidian_Frontmatter: 
-    def __init__(self, note_path: str) -> None:
-        # The path to the note is the note_path
-        self.note_path = note_path
-        self.content = self.get_content()
-        # Self.values is a dictionary of key-value pairs
-        self.values = {}
 
-        for line in self.content: 
+class Frontmatter: 
+    def __init__(self, note_title: str) -> None:
+        # Self.values is a dictionary of key-value pairs
+        self.values = {} 
+        self.content = self.get_content(note_title)
+        for line in content: 
             key, value = line.split(":")
             self.values[key] = value
-
+    self.title = self.values["title"]
     def add(self, key, value):
         self.values[key] = value 
 
 
-    def get_content(self):        
+
+    def get_content(self, note_path):        
         """ 
         Returns the content of the frontmatter.  
         """
-        with open(self.note_path, "r")as file:
+        with open(note_path, "r")as file:
             for line in file:
                 count = 0
-                content = ""
                 if line.startswith("---"):
                     count += 1
-                    if count >= 2: 
-                        return content
+                    if count == 2: 
+                        return content              
                     else:
-                        content += line
                         continue
-            return content
-        return ""
+                content += line
 
-        ################### TMEPALTRTEJ DCXOXWE 
-    def get_frontmatter(self, context: str) -> dict|None:
-        """
-        Retreives the frontmatter metadata the defined in the frontmatter of the note.
-
-        Returns:
-            A frontmatter object for the note containing the metadata values of the note.
-        """
-        for line in self.content:
-            if line.startswith("---") and line.endswith("\n"):
-                beginning_line = line
-                for sline in self.content:
-                    if (
-                        sline.startswith("---")
-                        and sline.endswith("\n")
-                        and sline != beginning_line
-                    ):
-                        end_line = sline
-        # Now that we have the beginning and end of the frontmatter, we can check if the value is in the frontmatter.
-        inside = False
-        for line in content:
-            if line == beginning_line:
-                inside = True
-            if line == end_line:
-                inside = False
-            if inside:
-                # for each value in the frontmatter set frontmatter.value to said value.
-                for char in line:
-                    value = line.split(":")[0]
-                    data = line.split(":")[1]
-        return self.frontmatter
-
+        return content 
+        
--- src/obsidian/obsidian_note.py
+++ src/obsidian/obsidian_note.py
@@ -1,90 +1,58 @@
 """Import the OS module to operate using operating system of the operating computer."""
 import os
+from obsidian.obsidian_frontmatter import Frontmatter
 
-"""Import Obsidian Objects for Dealing with Files Used By Obsidian MD."""
-from obsidian.obsidian_frontmatter import Obsidian_Frontmatter
 from obsidian.obsidian_vault import Obsidian_Vault
 
 class Obsidian_Note():
     """
-    Note Object allowing for better interactions with Obsidian Markdown, local files.
-    Note Title - the title of the file/note defined by the path of the note/file. 
-    Note Content - the content of the file including the fontmatter of the note.
-    Note Body - the entire content of the file not including the frontmatter. 
-    Note Frontmatter - the content of an Note inside of "---\n"s at the top. 
+    Note object allowing for inteactions with markdown notes with the syntax of Obsidian for the AutoGPT plugin, AutoGPTObsidian.
+    Attributes:  | Description:
+    Note Title - the title of the file/note defined by the path of the note/file. Additively, the title of the note without the file extension aka `.md`.
+    Note Content - the entire content of the file/note including the frontmatter. 
+    Note Body - the content of the note without the frontmatter.
+    Note Frontmatter - the content of an Obsidian Note inside of "---"s at the front of a note or defined with dataview syntax
+    Note Meta data - the metadata fields and their corresponding values of the note/file 
     """
-    def __init__(self, note_path) -> None: 
+    def __init__(self, vault: Obsidian_Vault, note_path) -> None: 
         """
-        Creates the Obsidian_Note Object with the given path.
+        Initialize the Obsidian_Note object with the given parameters.
+
         Args:
-            path: The internal path of the note with respect 
-            to the vault_working_directory. (Not including the vault_directory)
+            path: The path of the note with respect to the vault_working_directory. (Not including the vault_directory)
         """
-        self.path = note_path
-        self.title = self.get_title(note_path)
-        self.frontmatter = self.get_frontmatter(note_path)
-        self.content = self.get_content(note_path)
-        self.body = self.get_body(note_path)
+        self.vault_name = os.getenv("OBSIDIAN_VAULT_NAME") 
+        self.vault = vault
+        self.path = vault.vault_directory + note_path
+        self.title = os.path.basename(self.path).replace(".md", "")  
+        self.frontmatter: Frontmatter
+        self.content = None
+        self.body = None
         self.update_note_attributes(self.content)
 
-    def get_body(self, note_path) -> str: 
+    def format_frontmatter(self) -> str:
         """ 
-        Get the Body of the content of the note without the frontmatter. 
-        Args: 
-            note_path: The path of the note to get the body of. 
-        Returns: 
-            body: The body of the note.
-        """ 
-        with open(note_path, "r") as note_file: 
-            note_content = note_file.read()
-        frontmatter = self.get_frontmatter(note_path)
-        note_body = note_content.replace(frontmatter.content, "")
-        return note_body
+        Formats the frontmatter of the note to be in a standard format as chosen by AutoGPTObsidian.
+        --- 
+        tags: [tag1, tag2, tag3]
+        aliases: [alias1, alias2, alias3]
+        summary: [summary]
+        ---
 
-    def get_content(self, note_path) -> str: 
-        """ 
-        Returns the content of the note outside of the frontmatter. 
-        Args: 
-            note_path: The path of the note to get the body of. 
-        Returns: 
-            content: The content of the note.
         """
-        with open(note_path, "r") as note_file: 
-            note_content = note_file.read()
-        return note_content
+        self.reload()
+        
 
-
-    def get_frontmatter(self, note_path) -> Obsidian_Frontmatter: 
-        """
-        Returns the frontmatter of the note. 
-        Args:
-            note_path: The path of the note. 
-        Returns:
-            The frontmatter of the note. 
-        """
-        return Obsidian_Frontmatter(note_path) 
-
-    def get_title(self, note_path) -> str: 
-        """
-        Returns the title of the note. 
-
-        Args:
-            note_path: The path of the note. 
-
-        Returns:
-            The title of the note. 
-        """
-        return os.path.basename(note_path)
 
     def reload(self) -> None: 
         """
-        Reloads the note content from the file at the note's path. 
+        Reloads the note content from the file at the given path. 
         """
-        self.title = self.get_title(self.path)
-        self.frontmatter = self.get_frontmatter(self.path)
-        self.content = self.get_content(self.path)
-        self.body = self.get_body(self.path)
-        self.update_note_attributes(self.content)
+        with open(self.path, "r") as f: 
+            self.content = f.read()
+        if self.content is None: 
+            self.content = ""
+        self.update_note_attributes(self.content): 
 
     def update_note_attributes(self, context:str) -> None: 
         """
@@ -92,25 +60,22 @@ class Obsidian_Note():
         Updates the following attributes: tags, incoming_links, outgoing_links, frontmatter, dataview_metadata, and dataview_metadata_fields 
         """
 
-        self.frontmatter = self.get_frontmatter(context)
+         ######FRONTMATTER###### 
+        self.frontmatter = get_frontmatter(context)
+         ######TAGS######
+        # Get the tags of the note
+        self.tags = get_tags(context)
 
-        self.tags = self.get_tags(context)
+         ######INCOMING LINKS######
+        self.incoming_links = get_incoming_links(context)
 
-        self.incoming_links = self.get_incoming_links(context)
+         ######OUTGOING LINKS######
+        self.outgoing_links = get_outgoing_links(context)
 
-        self.outgoing_links = self.get_outgoing_links(context)
+         ######CONTENT######
+        with open(self.path, "r") as file: 
+            self.content = file.read()
 
-
-
-    def get_frontmatter(self, note_path) -> Obsidian_Frontmatter: 
-        """ 
-        Retreives the frontmatter of the note defined in Frontmatter syntax. 
-        Args: 
-            note_path: The path of the note. 
-        Returns: 
-            An Obsidian_Frontmatter object.
-        """
-        return Obsidian_Frontmatter(note_path)
 
     def get_tags(self, content) -> list: 
         """
@@ -141,6 +106,107 @@ class Obsidian_Note():
 
         return tags
 
+    def get_frontmatter(self, context: str) -> dict|None:
+        """
+        Retreives the frontmatter metadata the defined in the frontmatter of the note.
+
+        Returns:
+            A frontmatter object for the note containing the metadata values of the note.
+        """
+        for line in self.content:
+            if line.startswith("---") and line.endswith("\n"):
+                beginning_line = line
+                for sline in self.content:
+                    if (
+                        sline.startswith("---")
+                        and sline.endswith("\n")
+                        and sline != beginning_line
+                    ):
+                        end_line = sline
+        # Now that we have the beginning and end of the frontmatter, we can check if the value is in the frontmatter.
+        inside = False
+        for line in content:
+            if line == beginning_line:
+                inside = True
+            if line == end_line:
+                inside = False
+            if inside:
+                # for each value in the frontmatter set frontmatter.value to said value.
+                for char in line:
+                    value = line.split(":")[0]
+                    data = line.split(":")[1]
+        return self.frontmatter
+
+    def get_value_in_frontmatter(self, value):
+        """
+        Get the predefined value in the frontmatter of the note.
+
+        Parameters:
+            value: The value to get from the frontmatter.
+
+        Returns:
+            The value in the frontmatter. If the value is not in the frontmatter, returns None.
+        """
+        self.get_frontmatter()
+        return frontmatter.value
+
+    def is_value_in_content_as_dataview(self, value):
+        """
+        Determines whether the value is in the content of the note as a dataview.
+
+        Parameters:
+            value: The value to check for in the content in dataview syntax.
+
+        Returns:
+            True if the value is in the content, False otherwise.
+        """
+        value += "::"
+        for line in content:
+            if line.startswith(value):
+                value = line.split("::")[0]
+                data = line.split("::")[1]
+                self.dataview_metadata.value = data
+
+
+    def add_tags(self, tags) -> str:
+        """
+        Add tags to the note.
+
+        Args:
+            tags: The tags to add to the note.
+        """
+
+
+
+    def add_aliases(self, aliases) -> str:
+        """
+        Add aliases to the note.
+
+        Args:
+            aliases: The aliases to add to the note.
+        """
+        if self.get_aliases() is not None:
+            self.aliases += aliases
+            update_note(self)
+            return "Aliases added to note."
+
+    def set_summary(self, new_summary) -> str:
+        """
+        Change the summary of the note. Changes the frontmatter.summary value to the given summary, and changes note's summary value to the given summary.
+
+        Args:
+            summary: The new summary of the note.
+        """
+        self.frontmatter.summary = summary
+
+        # Replace the old summary from the note's content with the new summary
+        self.content = self.content.replace(self.summary, new_summary)))
+        
+        # Do the same to the file 
+        with open(self.path, "w") as f:
+            f.write(self.content)
+            return "Summary changed successfully."
+       return "Summary change failed. The current path is " + self.path + " and the current summary is " + self.summary + " and the new summary is " + new_summary + "." 
 
     def run_core_template(self) -> Exception|bool:
         """
