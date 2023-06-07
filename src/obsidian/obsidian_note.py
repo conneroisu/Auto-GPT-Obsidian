f1cb6983e009fcb8aaaadc59f13b5a69f0846260
connero <conneroisu@outlook.com>
2023-06-07 00:07:47 -0500

    off

--- src/obsidian/obsidian_note.py
+++ src/obsidian/obsidian_note.py
@@ -30,7 +30,7 @@ class Obsidian_Note():
         self.body = None
         self.update_note_attributes(self.content)
 
-    def format_frontmatter(self) -> str:
+#    def format_frontmatter(self) -> str:
         """ 
         Formats the frontmatter of the note to be in a standard format as chosen by AutoGPTObsidian.
         --- 
@@ -40,7 +40,7 @@ class Obsidian_Note():
         ---
 
         """
-        self.reload()
+#        self.reload()
         
 
 
@@ -52,27 +52,19 @@ class Obsidian_Note():
             self.content = f.read()
         if self.content is None: 
             self.content = ""
-        self.update_note_attributes(self.content): 
+        self.update_note_attributes(self.content)
 
     def update_note_attributes(self, context:str) -> None: 
         """
         Updates the attributes of the Obsidian_Note object based on the content of the note give as context, a string.
         Updates the following attributes: tags, incoming_links, outgoing_links, frontmatter, dataview_metadata, and dataview_metadata_fields 
         """
-
-         ######FRONTMATTER###### 
-        self.frontmatter = get_frontmatter(context)
-         ######TAGS######
+        ######FRONTMATTER###### 
+        self.frontmatter = self.get_frontmatter(context)
+        ######TAGS######
         # Get the tags of the note
-        self.tags = get_tags(context)
-
-         ######INCOMING LINKS######
-        self.incoming_links = get_incoming_links(context)
-
-         ######OUTGOING LINKS######
-        self.outgoing_links = get_outgoing_links(context)
-
-         ######CONTENT######
+        self.tags = self.get_tags(context)
+        ######CONTENT######
         with open(self.path, "r") as file: 
             self.content = file.read()
 
@@ -85,19 +77,16 @@ class Obsidian_Note():
             A list of tags for the note. 
         """
         tags = []
-
         ### FRONTMATTER ###
         for line in self.frontmatter.split("\n"): 
             if line.startswith("tags:"): 
                 tags = line.replace("tags:", "").strip().split(" ")
-
         ### CONTENT ###
         for line in content.split("\n"): 
             # split around spaces
             for word in line.split(" "): 
                 if word.startswith("#"): 
                     tags.append(word) 
-
         ### DATAVIEW ### 
         for line in content.split("\n"): 
             if line.startswith("tags::"): 
@@ -137,88 +126,7 @@ class Obsidian_Note():
                     data = line.split(":")[1]
         return self.frontmatter
 
-    def get_value_in_frontmatter(self, value):
-        """
-        Get the predefined value in the frontmatter of the note.
 
-        Parameters:
-            value: The value to get from the frontmatter.
-
-        Returns:
-            The value in the frontmatter. If the value is not in the frontmatter, returns None.
-        """
-        self.get_frontmatter()
-        return frontmatter.value
-
-    def is_value_in_content_as_dataview(self, value):
-        """
-        Determines whether the value is in the content of the note as a dataview.
-
-        Parameters:
-            value: The value to check for in the content in dataview syntax.
-
-        Returns:
-            True if the value is in the content, False otherwise.
-        """
-        value += "::"
-        for line in content:
-            if line.startswith(value):
-                value = line.split("::")[0]
-                data = line.split("::")[1]
-                self.dataview_metadata.value = data
-
-
-    def add_tags(self, tags) -> str:
-        """
-        Add tags to the note.
-
-        Args:
-            tags: The tags to add to the note.
-        """
-
-
-
-    def add_aliases(self, aliases) -> str:
-        """
-        Add aliases to the note.
-
-        Args:
-            aliases: The aliases to add to the note.
-        """
-        if self.get_aliases() is not None:
-            self.aliases += aliases
-            update_note(self)
-            return "Aliases added to note."
-
-    def set_summary(self, new_summary) -> str:
-        """
-        Change the summary of the note. Changes the frontmatter.summary value to the given summary, and changes note's summary value to the given summary.
-
-        Args:
-            summary: The new summary of the note.
-        """
-        self.frontmatter.summary = summary
-
-        # Replace the old summary from the note's content with the new summary
-        self.content = self.content.replace(self.summary, new_summary)))
-        
-        # Do the same to the file 
-        with open(self.path, "w") as f:
-            f.write(self.content)
-            return "Summary changed successfully."
-       return "Summary change failed. The current path is " + self.path + " and the current summary is " + self.summary + " and the new summary is " + new_summary + "." 
-
-    def run_core_template(self) -> Exception|bool:
-        """
-        Similar to the templates core plugin for Obsidian, this function tries to emulate the evaluation of {{date}} and {{title}}
-        """
-        for line in self.content:  
-                    if "{{date}}" in line: 
-                        line = line.replace("{{date}}", datetime.now().strftime("%m/%d/%Y"))
-                    if "{{title}}" in line: 
-                        line = line.replace("{{title}}", self.title) 
-                    # If the date and title come with javascript styled modifier  
-                    
-
-    def run_plugin_templater(self): 
+    def run_core_template(self):
+        # TODO: Run the core template.
         pass
